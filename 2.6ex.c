a5f32a05ea9d680f5bfa2deed22a9a6c8045ff46
khushi716 <khushieb25@gmail.com>
2025-01-30 23:19:51 +0530

    Update 2.6ex.c

--- 2.6ex.c
+++ 2.6ex.c
@@ -1,31 +1,20 @@
-/******************************************************************************
-
-                            Online C Compiler.
-                Code, Compile, Run and Debug C program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
-
 #include <stdio.h>
 #include<string.h>
 unsigned int set(unsigned int x, int p, int n,unsigned int y){
   int nbits = y&(~(~0<<n));
-  int shiftbits = nbits<<(p-n);
-  int clear = ~(~(~0<<n)<<(p-n));
-   x = x & clear;
-   return x | shiftbits;
+  int shiftbits = nbits<<(p-n+1);
+  int clear = ~(~(~0<<n)<<(p-n+1));
+  x = x & clear;
+  return x | shiftbits;
 }
 
 int main()
 {
-   int pp,nn;
-  unsigned int xx=240;
-  unsigned int yy=15;
-   pp = 3;
-   nn = 5;
-   set(xx,pp,nn,yy);
-   printf("%u",set(xx,pp,nn,yy));
+  int pp,nn;
+  unsigned int xx=0x2;
+  unsigned int yy=0xD;
+  pp = 2;
+  nn = 2;
+  set(xx,pp,nn,yy);
+  printf("%u",set(xx,pp,nn,yy));
 }
-
-
-

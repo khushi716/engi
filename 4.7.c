d542a3bc6717b30964e64d6ebe6618716d1e3d42
khushi716 <khushieb25@gmail.com>
2025-02-02 19:45:01 +0530

    4.7.c

--- 4.7.c
+++ 4.7.c
@@ -1,69 +1,125 @@
+#include <ctype.h>
 #include <stdio.h>
-#define MAX 1000
-#define BUFF 100
+#include <stdlib.h>
+
+#define MAXOP 100
+#define NUMBER '0'
+#define BUFSIZE 100
+#define MAXVAL 100
+
+char buf[BUFSIZE];
+int bufp = 0;
+int sp = 0;
+double val[MAXVAL];
+
+int getop(char[]);
+void push(double);
+double pop(void);
+
 int getch(void);
-void ungets(int c);
-void uget(char srt2[]);
-int getline_custom(char str[], int maxlimit);
+void ungetch(int);
 
-int main() {
-    char str1[MAX];
-    getline_custom(str1, MAX); 
-    int c; 
-    uget(str1);
-    while((c = getch())!= EOF){
-        putchar(c);
+/* reverse polish calculator */
+
+int main(void) {
+    int type, var = 0;
+    double op2, v;
+    char s[MAXOP];
+    double variable[26];
+
+    while ((type = getop(s)) != EOF) {
+        switch (type) {
+        case NUMBER:
+            push(atof(s));
+            break;
+        case '+':
+            push(pop() + pop());
+            break;
+        case '*':
+            push(pop() * pop());
+            break;
+        case '-':
+            op2 = pop();
+            push(pop() - op2);
+            break;
+        case '/':
+            op2 = pop();
+            if (op2 != 0.0)
+                push(pop() / op2);
+            else
+                printf("error:zero divisor\n");
+            break;
+        case '=':
+            pop();
+            if (var >= 'A' && var <= 'Z')
+                variable[var - 'A'] = pop();
+            else
+                printf("error: novariablename\n");
+            break;
+
+        case '\n':
+            v = pop();
+            printf("\t%.8g\n", v);
+            break;
+        default:
+            if (type >= 'A' && type <= 'Z')
+                push(variable[type - 'A']);
+            else if (type == 'v')
+                push(v);
+            else
+                printf("error: unknown command %s\n", s);
+            break;
+        }
+        var = type;
     }
-
     return 0;
 }
 
+void push(double f) {
+    if (sp < MAXVAL)
+        val[sp++] = f;
+    else
+        printf("error:stack full, cant push %g\n", f);
+}
 
-int getline_custom(char str[], int limit) {
-    int c, i;
-    
-   
-    for (i = 0; i < limit - 1 && (c = getchar()) != EOF && c != '\n'; i++) {
-        str[i] = c;
-    }
-    
-   
-    if (c == '\n') {
-        str[i] = c;
-        i++;  
-    }
-    
-  
-    str[i] = '\0';
-    
-    return i;
-}
-void uget(char str2[]){
-    int count = 0;
-    while(str2[count] != '\0'){
-        ++count;
-    }
-    while(count>0){
-        ungets(str2[--count]);
-    }
-}
-int buff = 0;
-int buffer[BUFF];
-void ungets(int c){
-    if(buff<=BUFF){
-        buffer[++buff]= c;
-    }
-    else{
-        printf("buffer is full");
+double pop(void) {
+    if (sp > 0)
+        return val[--sp];
+    else {
+        printf("error: stack empty\n");
+        return 0.0;
     }
 }
 
-int getch(void){
-    if(buff>0){
-        return buffer[--buff];
-    }
-    else{
-        return getchar();
-    }
-    
+int getop(char s[]) {
+    int i, c;
+
+    while ((s[0] = c = getch()) == ' ' || c == '\t')
+        ;
+    s[1] = '\0';
+    if (!isdigit(c) && c != '.')
+        return c;
+
+    i = 0;
+    if (isdigit(c))
+        while (isdigit(s[++i] = c = getch()))
+            ;
+
+    if (c == '.')
+        while (isdigit(s[++i] = c = getch()))
+            ;
+
+    s[i] = '\0';
+    if (c != EOF)
+        ungetch(c);
+    return NUMBER;
+}
+
+int getch(void) { return (bufp > 0) ? buf[--bufp] : getchar(); }
+
+void ungetch(int c) {
+    if (bufp >= BUFSIZE)
+        printf("ungetch: too many characters\n");
+    else
+        buf[bufp++] = c;
 }
